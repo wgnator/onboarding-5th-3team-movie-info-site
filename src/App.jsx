092f1f64b04e88bb8f6f3ee28c07513df3b8db34
Avoler0 <jys1391@gmail.com>
2022-07-06 23:12:28 +0900

    기초 세팅 재설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
+import React from 'react';
+import { useMovieModel } from './models/useMovieModel';
 import Router from './Router'
 
 function App() {
+  const { movies , getMovies} = useMovieModel();
+  React.useEffect(()=>{
+    getMovies()
+  },[])
   return (
         <Router />
   );
