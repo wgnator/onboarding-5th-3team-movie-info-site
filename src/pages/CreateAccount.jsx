24d843691512dd6a2414d0098a246935a944c651
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-07-07 15:53:04 +0900

    Feat: 로그인 상태일 때 login, createAccount로 접속하면 메인으로 리디렉션

--- src/pages/CreateAccount.jsx
+++ src/pages/CreateAccount.jsx
@@ -1,6 +1,16 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import CreateAccountForm from "../component/CreateAccountForm";
+import { getToken } from "../utils/library";
 
 function CreateAccount({ users }) {
+  const navigate = useNavigate();
+
+  const token = getToken();
+  useEffect(() => {
+    if (token) navigate("/");
+  }, []);
+
   return <CreateAccountForm users={users} />;
 }
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,16 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import LoginForm from "../component/LoginForm";
+import { getToken } from "../utils/library";
 
 function Login({ users }) {
+  const navigate = useNavigate();
+
+  const token = getToken();
+  useEffect(() => {
+    if (token) navigate("/");
+  }, []);
+
   return <LoginForm users={users} />;
 }
 
