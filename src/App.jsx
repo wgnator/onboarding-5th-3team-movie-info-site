2ce1ed4c674a9f516577ddc8f24abb6a7cdd4ab5
Avoler0 <91608021+Avoler0@users.noreply.github.com>
2022-07-08 17:41:37 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,10 @@
-import { useEffect } from "react";
-import { useState } from "react";
 import Router from "./Router";
-import AccessUserDB from "./models/AccessUserDB";
 
 
 function App() {
-  const [data, setData] = useState();
-  useEffect(() => {
-    getData();
-  }, []);
+  return <Router />;
 
-  async function getData() {
-    const data = await AccessUserDB.getUsers();
-    setData(data);
-  }
-  // return <Router />;
-
-  return <>{JSON.stringify(data)}</>;
+//   return <>{JSON.stringify(data)}</>;
 }
 
 export default App;
