b1745fcb37dfbca90ad906fc1dccbb703d46f1a1
YOUNGHO8762 <YOUNGHO8762@gmail.com>
2022-07-08 12:02:41 +0900

    Feat : 검색 페이지 구현

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,5 +1,37 @@
-import React from "react";
+import React, { useEffect } from 'react';
+import styled from 'styled-components';
+import { useParams } from 'react-router-dom';
+
+import { useMovieModel } from '../models/useMovieModel';
+
+import Thumbnail from '../component/Thumbnail';
 
 export default function Main() {
-  return <div>main</div>;
+  const { movies, searchMovies } = useMovieModel();
+
+  const { movieTitle } = useParams();
+
+  useEffect(() => {
+    searchMovies(movieTitle);
+  }, [movieTitle]);
+
+  if (!movies?.results.length) {
+    return <div>없음</div>;
+  }
+
+  return (
+    <ThumbnailList>
+      {movies?.results.map((movie) => (
+        <Thumbnail key={movie.id} movie={movie} />
+      ))}
+    </ThumbnailList>
+  );
 }
+
+const ThumbnailList = styled.ul`
+  width: 85vw;
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-gap: 20px;
+`;
