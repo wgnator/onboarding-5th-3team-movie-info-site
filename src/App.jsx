7b88b4e590f0b38c7e9be3f7b7c51bf976c57ff8
wgnator <wgnator@gmail.com>
2022-07-08 16:59:31 +0900

    Fix: HttpRequest 객체 오류해결

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
-
+import { useEffect } from "react";
+import { useState } from "react";
 import Router from "./Router";
+import AccessUserDB from "./models/AccessUserDB";
 
 function App() {
-  return <Router />;
+  const [data, setData] = useState();
+  useEffect(() => {
+    getData();
+  }, []);
+
+  async function getData() {
+    const data = await AccessUserDB.getUsers();
+    setData(data);
+  }
+  // return <Router />;
+
+  return <>{JSON.stringify(data)}</>;
 }
 
 export default App;
